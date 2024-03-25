28012ba007f403d65b3de17d98dfab0d3a552d9b
fhashimovaa <fidanhewimova@gmail.com>
2024-03-25 16:01:48 +0400

    update Account service

--- account-service/src/main/java/com/microservice/accountservice/configuration/CustomBeanFactory.java
+++ account-service/src/main/java/com/microservice/accountservice/configuration/CustomBeanFactory.java
@@ -1,14 +1,15 @@
 package com.microservice.accountservice.configuration;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CustomBeanFactory {
 
-//    @Bean
-//    public ModelMapper getModelMapper() {
-//        return new ModelMapper();
-//    }
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 
 }
