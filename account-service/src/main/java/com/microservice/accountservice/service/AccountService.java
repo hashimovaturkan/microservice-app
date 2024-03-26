f8180ca3d737bbb10e28c6f01c930d21c37284ec
fhashimovaa <fidanhewimova@gmail.com>
2024-03-26 17:10:55 +0400

    fix(config): docker

--- account-service/src/main/java/com/microservice/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/microservice/accountservice/service/AccountService.java
@@ -25,33 +25,30 @@ public class AccountService {
 
 
     public AccountDto get(String id) {
-//        Account account = accountRepository.findById(id)
-//                .orElseThrow(() -> new IllegalArgumentException());
-//        return modelMapper.map(account, AccountDto.class);
-        return null;
+        Account account = accountRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException());
+        return modelMapper.map(account, AccountDto.class);
     }
 
     @Transactional
     public AccountDto save(AccountDto accountDto) {
-//        Account account = modelMapper.map(accountDto, Account.class);
-//        account = accountRepository.save(account);
-//        accountDto.setId(account.getId());
-//        return accountDto;
-        return null;
+        Account account = modelMapper.map(accountDto, Account.class);
+        account = accountRepository.save(account);
+        accountDto.setId(account.getId());
+        return accountDto;
     }
 
     @Transactional
     public AccountDto update(String id, AccountDto accountDto) {
-//        Assert.isNull(id, "Id cannot be null");
-//        Optional<Account> account = accountRepository.findById(id);
-//        Account accountToUpdate = account.map(it -> {
-//            it.setBirthDate(accountDto.getBirthDate());
-//            it.setName(accountDto.getName());
-//            it.setSurname(accountDto.getSurname());
-//            return it;
-//        }).orElseThrow(IllegalArgumentException::new);
-//        return modelMapper.map(accountRepository.save(accountToUpdate), AccountDto.class);
-return null;
+        Assert.isNull(id, "Id cannot be null");
+        Optional<Account> account = accountRepository.findById(id);
+        Account accountToUpdate = account.map(it -> {
+            it.setBirthDate(accountDto.getBirthDate());
+            it.setName(accountDto.getName());
+            it.setSurname(accountDto.getSurname());
+            return it;
+        }).orElseThrow(IllegalArgumentException::new);
+        return modelMapper.map(accountRepository.save(accountToUpdate), AccountDto.class);
     }
 
     @Transactional
--- ticket-service/src/main/java/com/microservice/ticketservice/service/impl/TicketServiceImpl.java
+++ ticket-service/src/main/java/com/microservice/ticketservice/service/impl/TicketServiceImpl.java
@@ -1,27 +1,29 @@
 package com.microservice.ticketservice.service.impl;
 
 import com.microservice.ticketservice.dto.TicketDto;
+import com.microservice.ticketservice.model.PriorityType;
+import com.microservice.ticketservice.model.Ticket;
+import com.microservice.ticketservice.model.TicketStatus;
+import com.microservice.ticketservice.model.elasticSearch.TicketModel;
 import com.microservice.ticketservice.repository.TicketRepository;
 import com.microservice.ticketservice.repository.elasticSearch.TicketElasticRepository;
 import com.microservice.ticketservice.service.TicketNotificationService;
 import com.microservice.ticketservice.service.TicketService;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TicketServiceImpl implements TicketService {
 
-    //private final TicketElasticRepository ticketElasticRepository;
+    private final TicketElasticRepository ticketElasticRepository;
     private final TicketRepository ticketRepository;
     private final TicketNotificationService ticketNotificationService;
     //private final AccountServiceClient accountServiceClient;
 
-    public TicketServiceImpl(TicketRepository ticketRepository, TicketNotificationService ticketNotificationService) {
+    public TicketServiceImpl(TicketElasticRepository ticketElasticRepository, TicketRepository ticketRepository, TicketNotificationService ticketNotificationService) {
+        this.ticketElasticRepository = ticketElasticRepository;
         this.ticketRepository = ticketRepository;
         this.ticketNotificationService = ticketNotificationService;
     }
@@ -29,44 +31,42 @@ public class TicketServiceImpl implements TicketService {
     @Override
     @Transactional
     public TicketDto save(TicketDto ticketDto) {
-//        // Ticket Entity
-//        if (ticketDto.getDescription() == null)
-//            throw new IllegalArgumentException("Description bos olamaz");
-//
-//        Ticket ticket = new Ticket();
-//        ResponseEntity<AccountDto> accountDtoResponseEntity = accountServiceClient.get(ticketDto.getAssignee());
-//
-//        ticket.setDescription(ticketDto.getDescription());
-//        ticket.setNotes(ticketDto.getNotes());
-//        ticket.setTicketDate(ticketDto.getTicketDate());
-//        ticket.setTicketStatus(TicketStatus.valueOf(ticketDto.getTicketStatus()));
-//        ticket.setPriorityType(PriorityType.valueOf(ticketDto.getPriorityType()));
-//        ticket.setAssignee(accountDtoResponseEntity.getBody().getId());
-//
-//        // mysql kaydet
-//        ticket = ticketRepository.save(ticket);
-//
-//
-//        // TicketModel nesnesi yarat
-//        TicketModel model = TicketModel.builder()
-//                .description(ticket.getDescription())
-//                .notes(ticket.getNotes())
-//                .id(ticket.getId())
-//                .assignee(accountDtoResponseEntity.getBody().getNameSurname())
-//                .priorityType(ticket.getPriorityType().getLabel())
-//                .ticketStatus(ticket.getTicketStatus().getLabel())
-//                .ticketDate(ticket.getTicketDate()).build();
-//
-//        // elastic kaydet
-//        ticketElasticRepository.save(model);
-//
-//        // olusan nesneyi döndür
-//        ticketDto.setId(ticket.getId());
-//
-//        // Kuyruga notification yaz
-//        ticketNotificationService.sendToQueue(ticket);
-//        return ticketDto;
-        return null;
+
+        if (ticketDto.getDescription() == null)
+            throw new IllegalArgumentException("Description should not be empty");
+
+        Ticket ticket = new Ticket();
+        //ResponseEntity<AccountDto> accountDtoResponseEntity = accountServiceClient.get(ticketDto.getAssignee());
+
+        ticket.setDescription(ticketDto.getDescription());
+        ticket.setNotes(ticketDto.getNotes());
+        ticket.setTicketDate(ticketDto.getTicketDate());
+        ticket.setTicketStatus(TicketStatus.valueOf(ticketDto.getTicketStatus()));
+        ticket.setPriorityType(PriorityType.valueOf(ticketDto.getPriorityType()));
+        //ticket.setAssignee(accountDtoResponseEntity.getBody().getId());
+
+        // save mysql
+        ticket = ticketRepository.save(ticket);
+
+
+        // create TicketModel
+        TicketModel model = TicketModel.builder()
+                .description(ticket.getDescription())
+                .notes(ticket.getNotes())
+                .id(ticket.getId())
+                //.assignee(accountDtoResponseEntity.getBody().getNameSurname())
+                .priorityType(ticket.getPriorityType().getLabel())
+                .ticketStatus(ticket.getTicketStatus().getLabel())
+                .ticketDate(ticket.getTicketDate()).build();
+
+        // save elastic
+        ticketElasticRepository.save(model);
+
+        ticketDto.setId(ticket.getId());
+
+        // Queue notofication
+        ticketNotificationService.sendToQueue(ticket);
+        return ticketDto;
     }
 
     @Override
