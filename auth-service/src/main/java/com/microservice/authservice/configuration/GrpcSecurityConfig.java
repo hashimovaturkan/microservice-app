9e2b2a1a83ddaaf23157eb906a825d7bab045918
Turkan Hashimova <turkan.hashimova@azintelecom.az>
2024-04-18 18:11:30 +0400

    fix grpcAuthenticationReader

---
+++ auth-service/src/main/java/com/microservice/authservice/configuration/GrpcSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.microservice.authservice.configuration;
+
+import net.devh.boot.grpc.server.security.authentication.BasicGrpcAuthenticationReader;
+import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class GrpcSecurityConfig {
+
+    @Bean
+    public GrpcAuthenticationReader grpcAuthenticationReader() {
+        return new BasicGrpcAuthenticationReader();
+    }
+}
